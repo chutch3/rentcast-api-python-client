f137cea757a1d654d35de426855f6551c0644d03
Cody Hutchens <chutchens91@gmail.com>
2024-09-09 21:03:06 -0400

    feat: added package version

--- scripts/generate_package.sh
+++ scripts/generate_package.sh
@@ -3,6 +3,7 @@ set -e
 
 INPUT_SPEC="openapi_spec.json"
 PACKAGE_NAME="rentcast_client"
+PACKAGE_VERSION="1.0.1"
 OUTPUT_DIR="$PACKAGE_NAME"
 
 echo "Generating Python package from OpenAPI spec"
@@ -11,6 +12,7 @@ openapi-generator-cli generate \
     -g python \
     -o "$OUTPUT_DIR" \
     --package-name "$PACKAGE_NAME" \
+    --additional-properties=packageVersion="$PACKAGE_VERSION" \
     --library asyncio
 
 
